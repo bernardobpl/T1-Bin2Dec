5f4d6405fd387d7c03972aff634ddd73ffbaee6f
Jajack <bernardobpl97@gmail.com>
2020-05-04 19:59:13 -0300

    Binary to decimal convert logic

--- bin2dec/src/App.js
+++ bin2dec/src/App.js
@@ -1,29 +1,40 @@
 import React, { useState } from 'react';
+import './app.css';
 
 function App() {
   const [binary, setBinary] = useState('');
-  //const [decimal, setDecimal] = useState('');
+  const [decimal, setDecimal] = useState('');
   
 
-  function convert(){
-     //binary
-     // setDecimal(result);
+  function convert2dec(){
+    //input characters check
+    if (binary.match(/^[0-1]+$/g) === null){
+      alert('Only 0 and 1');
+      return
+    }
 
+    //logic
+    //use reverse() method so it isn't necessary to use string.lenght
+    let binaryint = binary.split('').reverse().map(Number);
+    let result = binaryint.reduce(
+      (sum, next, index) => (sum + next*(2**index))
+    );
+
+    setDecimal(result);
   }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Binary to Decimal </h1>
-      </header>
-      <div className="main">
-        <label> Binary: </label>
-        <input className="entry" type="text" placeholder="0 and 1" value={binary} onChange={e => setBinary(e.target.value)}/>
+        <h1> Binary to Decimal Convert</h1>
+
+        <label style={{marginLeft:'5px'}}> Binary: </label>
+        <input type="text" placeholder="0 and 1" value={binary} onChange={e => setBinary(e.target.value)}/>
+        <button onClick={convert2dec}> Convert to Dec</button>
+        <br/><br/>
+
         <label> Decimal: </label>
-        <input type="number" placeholder="Result"  readOnly/>
-        <input type="button" onClick={convert}/>
+        <input id="resultinp" type="number" placeholder="Result" value={decimal} readOnly/>
       </div>
-    </div>
   );
 }
 
